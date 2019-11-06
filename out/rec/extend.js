a615a193ba1e49d62cca3ecb885dfc751ba987da
s-nt-s <santos82h@gmail.com>
2019-11-06 09:41:41 +0100

    if (isUrlOnline(url)) return opt.when_url_exist.fire(opt);

--- out/rec/extend.js
+++ out/rec/extend.js
@@ -232,7 +232,7 @@ class WhenUrlExist {
 function my_ajax(url, opt) {
   if (!url) return $.ajax(opt);
   opt.when_url_exist = new WhenUrlExist(opt.form.attr("id"), url, null);
-  if (isUrlOnline(url)) return opt.when_url_exist.fire();
+  if (isUrlOnline(url)) return opt.when_url_exist.fire(opt);
   return $.ajax(opt).fail(function(data, textStatus, jqXHR) {
     //if (textStatus!="timeout") return;
     if (this.when_url_exist) {
