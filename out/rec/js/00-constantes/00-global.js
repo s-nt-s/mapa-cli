1b8642a06f2283eb603d013c1b41b57711f243cc
s-nt-s <santos82h@gmail.com>
2020-03-31 12:57:35 +0200

    trim index.html

--- out/rec/js/00-constantes/00-global.js
+++ out/rec/js/00-constantes/00-global.js
@@ -4,4 +4,5 @@ myweb = myweb.substr(document.location.protocol.length+2)
 if (myweb.endsWith("/")) myweb = myweb.substr(0, myweb.length-1);
 
 var myroot = window.location.href;
+if (myroot.endsWith("/index.html")) myroot = myroot.substr(0, myweb.length-10);
 if (!myroot.endsWith("/")) myroot = myroot+"/";
