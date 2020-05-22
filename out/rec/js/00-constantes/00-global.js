7e330e535aa303ec7cd6eab64587baf5ec03eee1
s-nt-s <santos82h@gmail.com>
2020-05-22 12:38:41 +0200

    añadir / al final de la url

--- out/rec/js/00-constantes/00-global.js
+++ out/rec/js/00-constantes/00-global.js
@@ -1,4 +1,11 @@
 var myweb = window.location.href;
+
+var tail = myweb.split(/\//);
+tail = tail[tail.length-1];
+if (tail.length>0 && tail.indexOf(".")==-1) {
+    window.location.href = window.location.href + "/";
+}
+
 if (myweb.endsWith("/index.html")) myweb = myweb.substr(0, myweb.length-11);
 myweb = myweb.substr(document.location.protocol.length+2)
 if (myweb.endsWith("/")) myweb = myweb.substr(0, myweb.length-1);
