f3cc6a2d91bc16b6857e8ac0628aa21c84dfba0a
s-nt-s <santos82h@gmail.com>
2020-05-25 14:19:31 +0200

    mostrar de cuando son los datos de aemet

--- out/rec/js/01-constantes/prediccion_semanal.js
+++ out/rec/js/01-constantes/prediccion_semanal.js
@@ -6,4 +6,13 @@ $.getJSON("https://dataia.mapa.gob.es/data-municipios/aemet/prediccion_semanal.j
 ) {
   prediccion_semanal = data;
   logHttp(this.url, http, prediccion_semanal["__timestamp__"]);
+  if (prediccion_semanal["__timestamp__"]) {
+    $(document).ready(function() {
+        var d=new Date(0)
+        d.setUTCSeconds(prediccion_semanal["__timestamp__"]);
+        var eq = $("span.datos_aemet_prediccion_semanal");
+        eq.html("aemet (obtenidos hace "+intervalo(d, true)+")");
+        eq.attr("title", getStrFecha(d))
+    });
+  }
 });
