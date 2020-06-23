700939023456c216b34d888c1053fe0a50e0c795
s-nt-s <santos82h@gmail.com>
2020-06-23 16:47:36 +0200

    evitar bucle infinito por evitar cache

--- out/rec/js/02-extensiones/01-js_extend.js
+++ out/rec/js/02-extensiones/01-js_extend.js
@@ -1,6 +1,7 @@
 $(document).ajaxError(function (event, xhr, ajaxOptions, thrownError) {
   var ping_url = myroot+"ping.txt";
-  if (ajaxOptions.url == ping_url) {
+  var origin = ajaxOptions.url.replace(/\?[^\?]*$/, "");
+  if (origin == ping_url) {
     alert("Su sesión ha caducado.");
     location.reload();
     return;
