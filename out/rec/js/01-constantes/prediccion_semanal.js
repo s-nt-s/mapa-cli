7d921d6fe6f4861bc6ab75b5d04f9bdbb5c85629
s-nt-s <santos82h@gmail.com>
2020-09-08 12:28:50 +0200

    Usar __elaborado__ si existe

--- out/rec/js/01-constantes/prediccion_semanal.js
+++ out/rec/js/01-constantes/prediccion_semanal.js
@@ -6,13 +6,20 @@ $.getJSON("https://dataia.mapa.gob.es/data-municipios/aemet/prediccion_semanal.j
 ) {
   prediccion_semanal = data;
   logHttp(this.url, http, prediccion_semanal["__timestamp__"]);
-  if (prediccion_semanal["__timestamp__"]) {
+  var time = prediccion_semanal["__elaborado__"] || prediccion_semanal["__timestamp__"];
+  if (time) {
     $(document).ready(function() {
-        var d=new Date(0)
-        d.setUTCSeconds(prediccion_semanal["__timestamp__"]);
+        var d=new Date(0);
+        var elab = prediccion_semanal["__elaborado__"];
+        var time = elab || prediccion_semanal["__timestamp__"];
+        d.setUTCSeconds(time);
         var eq = $("span.datos_aemet_prediccion_semanal");
         var hace = seconds_to_hours(intervalo(d, false));
-        eq.html("aemet (obtenidos hace <span>"+hace+"</span>)");
+        if (elab) {
+          eq.html("aemet (elaborados hace <span>"+hace+"</span>)");
+        } else {
+          eq.html("aemet (obtenidos hace <span>"+hace+"</span>)");
+        }
         eq.find("span").attr("title", getStrFecha(d))
     });
   }
