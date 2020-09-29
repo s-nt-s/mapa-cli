bed27045ed73880ec8edf3529f8d6e39f56b0815
s-nt-s <santos82h@gmail.com>
2020-09-29 09:52:56 +0200

    recuperar https://dataia.mapa.gob.es/egif/meta.json sincronamente

--- out/rec/js/01-constantes/meta.js
+++ out/rec/js/01-constantes/meta.js
@@ -1,4 +1,5 @@
 var meta_info={};
+/*
 $.getJSON("https://dataia.mapa.gob.es/egif/meta.json", function(
   data,
   status,
@@ -6,4 +7,18 @@ $.getJSON("https://dataia.mapa.gob.es/egif/meta.json", function(
 ) {
   meta_info = data;
   logHttp(this.url, http);
+});
+*/
+$.ajax({
+  url: "https://dataia.mapa.gob.es/egif/meta.json",
+  dataType: 'json',
+  async: false,
+  success: function(
+    data,
+    status,
+    http
+  ) {
+    meta_info = data;
+    logHttp(this.url, http);
+  }
 });
